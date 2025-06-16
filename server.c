108b145d3714b1fc64b1fa9ef8c4757e780358aa
Ofek <ofek.israel@campus.technion.ac.il>
2025-06-16 10:50:36 +0300

    Fixing a small bug

--- server.c
+++ server.c
@@ -23,7 +23,7 @@ void* workerThread(void* arg) {
     int id = *(int*)arg;
 
     threads_stats t = malloc(sizeof(struct Threads_stats));
-    t->id = id;
+    t->id = id + 1;
     t->stat_req = 0;       // Static request count
     t->dynm_req = 0;       // Dynamic request count
     t->total_req = 0;      // Total request count
